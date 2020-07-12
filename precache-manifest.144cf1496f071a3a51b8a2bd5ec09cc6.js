860e3d03fdbb54d5539313037a7446cc4668749c
Sonia Meller <mellersonia@gmail.com>
2020-07-12 15:24:51 +0100

    Updates

---
+++ precache-manifest.144cf1496f071a3a51b8a2bd5ec09cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b36e59031e26724e68cc63b72a816a",
+    "url": "/twitter-front/index.html"
+  },
+  {
+    "revision": "54f2516c9ca5c45f3d6f",
+    "url": "/twitter-front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88fd353e540d2c9947a7",
+    "url": "/twitter-front/static/js/2.c9fd3033.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/twitter-front/static/js/2.c9fd3033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f2516c9ca5c45f3d6f",
+    "url": "/twitter-front/static/js/main.f30a56fb.chunk.js"
+  },
+  {
+    "revision": "c32896484a34ba24bbe4",
+    "url": "/twitter-front/static/js/runtime-main.79179cd7.js"
+  }
+]);
